967a97e24927fa6b0ddfb9b07a1ce1d42afd8dd0
Elizabeth Edminster <109642264+lizedminster@users.noreply.github.com>
2024-02-17 18:16:22 -0500

    Slot Machine Origin

---
+++ Java Slot Machine/Java Slot Machine/src/Slot.java
@@ -0,0 +1,17 @@
+public class Slot {
+    public static final String Cash = "\ud83d\udcb5";
+    public static final String Fruit = "\uD83C\uDF52";
+    public static final String Star = "\u272f";
+    public static final String Spade = "\ud83c\udca1";
+    public static final String Clover = "\ud83c\\udf40";
+
+    String[] machine; 
+
+    int input = 0;
+    int constant = 0;
+    int output = input*constant;
+
+    Slot(){
+        
+    }
+}
