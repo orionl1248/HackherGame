e7f04d8a4c854ce489332878368585c713acf37d
Elizabeth Edminster <109642264+lizedminster@users.noreply.github.com>
2024-02-17 19:09:11 -0500

    Slot Machine Randomizes
    The Slot Machine can randomize now. No constant determination for winnings.

--- Java Slot Machine/Java Slot Machine/src/App.java
+++ Java Slot Machine/Java Slot Machine/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Slot n = new Slot(2);
+        n.move();
+        System.out.println(n);
     }
 }
