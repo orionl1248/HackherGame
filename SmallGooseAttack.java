5c400aae320c22ec08e1c3ee2520a9e3cb9f24c3
Thatonboi <yychen97255@gmail.com>
2024-02-18 00:47:06 -0500

    Added 2 more Duck image
    DUCK DUCK GOOOSEE

---
+++ SmallGooseAttack.java
@@ -0,0 +1,26 @@
+public class SmallGooseAttack {
+    public static void main(String[] args) {
+        System.out.println("                      /|                           ");
+        System.out.println("           _|__      / |     /|   _---_            ");
+        System.out.println("         -/-+-/ \\___/  /    / |  |____|\\___        ");
+        System.out.println("          \\ | ----- --      |_|__/ \\--+--          ");
+        System.out.println("           \\   \\  /           ------  |/           ");
+        System.out.println("|\\ ____----/    \\                 /  /----_____|\\  ");
+        System.out.println(" \\           |   |              /               _/ ");
+        System.out.println("  \\     ____/   |     _______   |     \\____/   /   ");
+        System.out.println("    \\___    __ /     / _   _ \\   \\ _         _/    ");
+        System.out.println("       |[  |[       | / \\_/ \\ |      ||---||       ");
+        System.out.println("       ---  ---     ||!_/ \\_!||    ===   ===       ");
+        System.out.println("              ===    \\<_____>/      /|             ");
+        System.out.println("              |  |\\   \\     /      |_/             ");
+        System.out.println("              |  || __/     \\__    /               ");
+        System.out.println("              |  ||/           \\_ /                "); 
+        System.out.println("              | /||               \\                ");
+        System.out.println("              || |||           |   |               ");
+        System.out.println("              | \\||/            \\ /                ");
+        System.out.println("              |  //_    ___    _/                  ");
+        System.out.println("              ===    ||     ||                     ");
+        System.out.println("                   ====     ====                   ");
+    } 
+    }
+    
