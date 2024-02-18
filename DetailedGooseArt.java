f6054509be6c08b3553e6da4bbd9222a0db01625
Thatonboi <yychen97255@gmail.com>
2024-02-17 22:58:31 -0500

    Added Goose

---
+++ DetailedGooseArt.java
@@ -0,0 +1,25 @@
+public class DetailedGooseArt {
+    public static void main(String[] args) {
+        System.out.println("        +++++++                  ");
+        System.out.println("     ++++++++++++                ");
+        System.out.println("    +++    +++++++               ");
+        System.out.println("    ++++++++++++++++++++         ");
+        System.out.println("    ++          ++++++           ");
+        System.out.println("     |___    ||| |               ");
+        System.out.println(" ____/   |__     |               ");
+        System.out.println("/__________/    /                ");
+        System.out.println("       |       |                 ");
+        System.out.println("       /       |                 ");
+        System.out.println("    __/         \\____        _/\\ ");
+        System.out.println("  _/                 \\    __/   |");
+        System.out.println(" /                    \\_/      | ");
+        System.out.println("|         /                   /  ");
+        System.out.println("|        |                   |  ");
+        System.out.println(" \\        |________/       __/   ");
+        System.out.println("   \\                 ____/       ");
+        System.out.println("     \\ _____________/            ");
+        System.out.println("        | |      | |             ");
+        System.out.println("     ___| |   ___| |             ");
+        System.out.println("     -----    -----              ");
+    }
+}
